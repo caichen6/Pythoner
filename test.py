9aeb88b5bea7ad29acca820744ae35aae57a9448
caichen <caichen@CaiMacBook.local>
2016-03-30 21:52:29 +0800

    new

--- test.py
+++ test.py
@@ -2,6 +2,11 @@
 print 'Hello Atom!'
 import os
 print os.popen('which python').readlines()
+
+'''
 for i in range(1,5):
     cmd = 'ping 192.168.0.'+str(i)
-    print cmd+':'+os.popen(cmd).readlines()
+    print cmd
+    print os.popen(cmd).readlines()
+
+'''
